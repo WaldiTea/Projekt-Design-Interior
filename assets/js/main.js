7cf4496eb2cc7e8035a7bfeaf655c246d9b95f96
SuperCoder <supercoder@MacBook-Air-von-SuperCoder.local>
2022-02-02 21:02:52 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let inputName = document.getElementById('input-name');
+let inputEmail = document.getElementById('input-email');
+let inputMessage = document.getElementById('input-message');
+let submitButton = document.getElementById('submit-button');
+
+function transfer() {
+  console.log(inputName.value);
+  console.log(inputEmail.value);
+  console.log(inputMessage.value);
+}
